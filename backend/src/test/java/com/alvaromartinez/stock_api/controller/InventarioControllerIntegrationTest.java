fb42f4956514a03dc615045030cc8fda33e1710c
amartinezdev <alvaromartinezdev@gmail.com>
2026-07-29 23:45:31 +0200

    añade y finaliza los test de integración con Inventario

--- backend/src/test/java/com/alvaromartinez/stock_api/controller/InventarioControllerIntegrationTest.java
+++ backend/src/test/java/com/alvaromartinez/stock_api/controller/InventarioControllerIntegrationTest.java
@@ -18,7 +18,6 @@ import org.springframework.transaction.annotation.Transactional;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
